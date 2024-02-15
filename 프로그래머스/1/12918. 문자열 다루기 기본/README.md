59cb1948dea859b030638d02b3c1d755ea9b1fad
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-02-15 14:55:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 2.49 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 0:26:47
+2024년 2월 4일 14:55:5
 
 ### 문제 설명
 
--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,26 +1,29 @@
-function solution(s) {
+/*function solution(s) {
     
     let changeToNumber = Number(s);
-    //console.log(s);
+    //console.log(s.split(""));
    
-    console.log(changeToNumber+1);
+    console.log(changeToNumber);
     //console.log(Boolean(changeToNumber+1));
     //console.log(typeof(changeToNumber+1));
     
     if(!(s.includes('e') || s.includes('x')) 
        && (s.length === 4 || s.length === 6)) {
-        if(Number.isInteger(changeToNumber) === true) return true;
+        /*if(Number.isInteger(changeToNumber) === true) return true;
         else if(Number.isInteger(changeToNumber) === false) return false;
     } else return false;
     
-    
-    
-  /* for(i=0; i<s.length; i++) {
-        if(typeof([i]) === 'number') {
-            return true;
-            
-        //console.log(typeof(Number("a")));
-        
-        } else return false;
-    }*/
+    if(isNaN(changeToNumber) === true) return false;
+        else if(isNaN(changeToNumber) === false) return true;
+    } else return false;
+}*/
+
+
+function solution(s) {
+
+    if(s.length === 4 || s.length === 6){
+        return s.split("").every(e => !isNaN(e)) // 모든 요소가 number이면 true리턴
+    } else {
+        return false;
+    }
 }
