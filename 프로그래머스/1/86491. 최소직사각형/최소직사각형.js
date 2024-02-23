cdf9e5d3cb73360d5b7e506a828f2ba6e9425ac3
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-02-23 17:33:47 +0900

    [level 1] Title: 최소직사각형, Time: 2.64 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,18 @@
+function solution(sizes) {
+    let width = 0; //80
+    let height = 0; //50
+    
+    for(let i = 0; i < sizes.length; i++){
+        if(sizes[i][0] < sizes[i][1]){
+            sizes[i].reverse(); 
+        }
+        if(sizes[i][0] > width){
+            width = sizes[i][0];
+        }
+        if(sizes[i][1] > height){
+            height = sizes[i][1]; 
+        }
+    }
+    
+    return width * height;
+}
