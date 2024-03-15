baddae339d5f4b708b015b62e3749f9162b67eb5
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-03-15 12:32:27 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,13 +1,13 @@
 function solution(numbers) {
-  let res = []
+  let ans = []
 
   for(let i=0; i<numbers.length; i++){
     for(let j=i+1; j<numbers.length; j++){
       for (let k=j+1; k<numbers.length; k++){
-        res.push(numbers[i]+numbers[j]+numbers[k])
+        ans.push(numbers[i]+numbers[j]+numbers[k])
       }
     }
   }
-  let result = res.filter((re) => re === 0).length
-  return result
+  let answer = ans.filter((re) => re === 0).length
+  return answer;
 }
