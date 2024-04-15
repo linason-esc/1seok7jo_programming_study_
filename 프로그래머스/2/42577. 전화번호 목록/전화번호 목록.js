4502cfd303014bd5902293601fd0197620afd729
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-04-16 08:33:34 +0900

    [level 2] Title: 전화번호 목록, Time: 179.49 ms, Memory: 95.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,27 @@
+function solution(phone_book) {
+    let hash = new Map();
+    let answer = true;
+    
+    /*for (i=0; i<phone_book.length; i++) {
+        hash.set(i, phone_book[i]);
+    }*/
+       phone_book.forEach(e=> {
+        hash.set(e, '');
+    })
+    //console.log(hash);
+    
+    for(let [key, value] of hash) {
+        for(let i=1; i<key.length; i++) {
+            let pre = key.slice(0, i);
+            //console.log(pre);
+
+            // 접두어를 확인하기 위한 문자열 자르는데 최소한의 길이는 1이 필요
+            // 해당 접두어가 해시에 존재하면 early return
+            if(hash.has(pre)) {
+                return false;
+            }
+        }
+    }
+
+    return answer;
+}
