610990740330d643a88b5cfb988e1efa936bd43d
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-05-10 11:06:06 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,8 @@
+function solution(citations) {
+    let answer = citations;
+    answer.sort((a,b)=>b-a);
+    for(let i = 0;i<answer.length;i++){
+        if(i>=answer[i]) return i;
+    }
+    return answer.length;
+}
