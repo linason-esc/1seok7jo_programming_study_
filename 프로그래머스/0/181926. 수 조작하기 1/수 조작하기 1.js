70a6743e09f89c17635bf0df05a3a414e097a96c
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-07-23 15:58:53 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.90 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,3 +1,4 @@
+// 내 처음 풀이
 function solution(n, control) {
     
     let controlArray = [...control];
@@ -17,4 +18,45 @@ function solution(n, control) {
     
     return n;
 
+}
+
+// 두번째 풀이
+// 개선점 1 : 문자열을 배열로 변환하지 않고 바로 접근하기 때문에 메모리 사용량이 줄어듦
+// 개선점 2 : switch 문을 사용하여 각 경우를 처리하면 가독성이 향상됨
+function solution(n, control) {
+    for (let i = 0; i < control.length; i++) {
+        switch (control[i]) {
+            case 'w':
+                n += 1;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+        }
+    }
+    return n;
+}
+
+// 세번째 풀이
+// 각 문자가 가지는 값을 미리 객체에 저장해 두고, 반복문을 통해 값을 더하거나 빼는 방식
+// 각 문자가 가지는 값을 미리 정의하여, 반복문 내에서 바로 적용할 수 있도록 하여, 코드의 간결성과 유지보수성을 높임
+function solution(n, control) {
+    const operations = {
+        'w': 1,
+        's': -1,
+        'd': 10,
+        'a': -10
+    };
+
+    for (let i = 0; i < control.length; i++) {
+        n += operations[control[i]];
+    }
+    
+    return n;
 }
