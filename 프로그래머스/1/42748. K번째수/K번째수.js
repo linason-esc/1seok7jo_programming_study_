3363515207d7c30038d6558f25e6ccbc2bc3cb06
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-07-25 10:40:57 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,4 +1,11 @@
 function solution(array, commands) {
-    var answer = [];
+    let answer = [];
+    for (i=0; i<commands.length; i++) {
+        let newArray = array.slice(commands[i][0]-1, 
+                                   commands[i][1]).sort((a,b) => a-b);
+
+        answer.push(newArray[commands[i][2]-1]);
+    }
+    
     return answer;
 }
