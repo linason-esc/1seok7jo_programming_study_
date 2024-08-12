2ba7a3de340f250c067ef6b3495b581c4820d5de
linason-esc <67824465+linason-esc@users.noreply.github.com>
2024-08-12 21:30:24 +0900

    [level 2] Title: 구명보트, Time: 70.19 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,12 +1,16 @@
+// people 배열을 가벼운 사람부터 순차적으로 정렬한다.
+// 제일 가벼운 사람(배열의 첫 번째) + 제일 무거운 사람(배열의 마지막 번째)의 합계가 `limit`보다 작거나 같으면 배열의 첫 번째와 마지막 요소를 제거하고 카운트를 증가시킨다.
+// 그렇지 않다면(위 두 사람의 합계가 limit을 초과한다면), 제일 무거운 사람부터 보트에 태우기 위해 배열의 마지막 요소를 제거하고 카운트를 증가시킨다.
+
 function solution(people, limit) {
     let count = 0;
-    people.sort((a, b) => a - b);
+    people.sort((a, b) => a - b); 
     
     while (people.length) {
         if (people[0] + people.at(-1) <= limit) {
             people.shift();
             people.pop();
-            count++;
+            count++; //3
         } else {
             people.pop();
             count++;
